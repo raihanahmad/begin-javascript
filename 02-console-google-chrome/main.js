479d61d368a3727f237857142b751f85ac9f43b6
Raihan Ahmad <61786331+raihanahmad@users.noreply.github.com>
2020-04-24 23:29:57 +0600

    I wrote my first code with javascript
    google chrome console

---
+++ 02-console-google-chrome/main.js
@@ -0,0 +1,52 @@
+// show log to console
+console.log(12563);
+console.log("raihan ahmad");
+console.log(false);
+console.log({
+  a: 9,
+  h: 49,
+  p: 4
+});
+console.table({
+  t: 4,
+  r: 1,
+  p: 94
+});
+// js first variable 
+var myname = 'raihan ahmad';
+console.log(myname);
+console.log([9, 8, 5, 3]);
+console.error('This is an error');
+console.warn('now this is a warning');
+console.clear();
+
+console.time('time');
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.log(myname);
+console.timeEnd('time');
